a0d571882e8c4190ce64387d91eaf56ee82448ef
Roman Mukhin <romamuhino1@gmail.com>
2023-08-26 01:09:49 +0300

    add options for qr generator

--- js/qr.js
+++ js/qr.js
@@ -1,20 +1,27 @@
 const defaultSettings = {
-    render: 'image',
-    size: 300,
-    ecLevel: 'H',
-    fill: '#212121',
+    radius: 0,
+    render: 'canvas',
+    size: 1000,
+    ecLevel: 'L',
+    fill: '#000000',
     text: 'URL goes here',
     mode: 0,
-    fontcolor: '#e41b17'
+    fontcolor: '#000000',
+    background:'#fff',
+    quiet: 2,
 }
-var type = 'text'
-sendData = (data, type) => {
+var currentType = 'text'
+var currentData = 'Hello, World!'
+
+const generateQr = (data, type) => {
+    $("#myQRCode").empty()
     switch (type) {
         case 'text':
+            data = !document.querySelector('input').value ? currentData : document.querySelector('input').value
             $("#myQRCode").qrcode(
-                {defaultSettings, text: data}
+                {...defaultSettings, text: data}
             );
-            console.log('hello world')
+            console.log({...defaultSettings, text: data})
 
             break;
     
@@ -22,6 +29,33 @@ sendData = (data, type) => {
             break;
     }
 }
-document.querySelector('input').addEventListener('keydown', function() {
-    sendData()
-  })
+
+const changeRadius = (radius) => {
+    defaultSettings['radius'] = radius/100
+    generateQr(currentData, currentType)
+}
+const changeColor = (color, id) =>{
+    if (id == 'qrBgColor'){
+        defaultSettings['background'] = color
+
+    } else {
+        defaultSettings['fill'] = color
+    }
+    generateQr(currentData, currentType)
+}
+document.querySelectorAll('.option-item').forEach(btn => {
+    btn.addEventListener("click", () => {
+        defaultSettings['ecLevel'] = 'H'
+        defaultSettings['mSize']= 0.2
+        defaultSettings['mode'] = 4
+        defaultSettings['image'] = btn.children[0]
+        document.getElementById('myQRCode').focus()
+        document.getElementById('myQRCode').click()
+
+        document.getElementById('logoBtn').style.backgroundImage = `url(${btn.children[0].src})`; 
+        console.log(btn.children[0].src)
+        generateQr(currentData, currentType)
+      });
+});
+
+generateQr(currentData, currentType)
